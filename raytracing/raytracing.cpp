8dd2d7cfadc7de8e3ec5932d2daa81098c95560d
raphaelchalicarne <raphael.chalicarne@ecl17.ec-lyon.fr>
2021-01-27 08:57:14 +0100

    Integrate Cos

--- raytracing/raytracing.cpp
+++ raytracing/raytracing.cpp
@@ -10,6 +10,13 @@
 #include "stb_image.h"
 
 #define M_PI 3.141592653589793238462
+
+#include <iostream>
+#include <random>
+
+static std::default_random_engine engine(10);
+static std::uniform_real_distribution<double> uniform(0,1);
+
 class Vector{
 public:
 	explicit Vector(double x = 0., double y = 0., double z = 0.){
@@ -134,6 +141,20 @@ public:
 	};
 };
 
+void integrateCos() {
+	int N = 10000;
+	double sigma = 0.25;
+	double s = 0;
+	for (int i=0; i < N; i++){
+		double u1 = uniform(engine);
+		double u2 = uniform(engine);
+		double xi = sigma*cos(2*M_PI*u1)*sqrt(-2*log(u2));
+		double p = 1/(sigma*sqrt(2*M_PI)) * exp(-xi*xi / (2*sigma*sigma));// Loi de probabilité gaussienne
+		s += pow(cos(xi), 10)/p/N;	// x_i
+	}
+	std::cout << s << std::endl;
+}
+
 int main() {
 	int W = 512;
 	int H = 512;
@@ -187,6 +208,8 @@ int main() {
 		}
 	}
 	stbi_write_png("image.png", W, H, 3, &image[0], 0);
+
+	integrateCos();
 
 	return 0;
 }
